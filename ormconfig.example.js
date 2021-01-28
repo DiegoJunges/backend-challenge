5a5835ea9eb3774496d8b13ba506987799b12c55
Diego Junges <diegojunges@hotmail.com>
2021-01-28 11:59:27 -0300

    env update

--- ormconfig.example.js
+++ ormconfig.example.js
@@ -1,16 +1,23 @@
 module.exports = {
-  type: '',
-  host: '',
-  username: '',
-  password: '',
-  database: '',
+  type: process.env.POSTGRES_TYPE,
+  host: process.env.POSTGRES_HOST,
+  "extra": {
+    "socketPath": process.env.POSTGRES_HOST
+  },
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABAASE,
+  logging: false,
+  synchronize: true,
+
   entities: [
-    "./dist/modules/**/infra/typeorm/entities/*.js"
+    "dist/modules/**/infra/typeorm/entities/*.js"
   ],
   migrations: [
-    "./dist/shared/infra/typeorm/migrations/*.js"
+    "dist/shared/infra/typeorm/migrations/*.js"
   ],
   cli: {
-    "migrationsDir": "./dist/shared/infra/typeorm/migrations"
+    "migrationsDir": "dist/shared/infra/typeorm/migrations"
   }
 }
+
